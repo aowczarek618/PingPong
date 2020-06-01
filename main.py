6a101ffd6b32bbef990a5624e198fa4de616ecd7
Artur Owczarek <oartur618@gmail.com>
2020-06-01 22:46:57 +0200

    3.0

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import turtle
 import os
-import random
+import sys
 import time
 
 import players
@@ -25,19 +25,40 @@ def paddle_ball_collisions(player, ball):
 
 
 def main():
-
+    """
+    Checking a platform type (Linux, Mac OS) to choose a proper play sound command.
+    If you want to play sound on Windows you have to import winsound module and use
+    winsound.PlaySound("bounce.wav", winsound.SND_ASYNC) function then. Unfortunately I can't do it on Linux.
+    """
+    if sys.platform == "Linux":
+        play_sound = "aplay -q bounce.wav&"
+    elif sys.platform == "Darwin":
+        play_sound = "afplay -q bounce.wav&"
+    elif sys.platform == "Windows":
+        pass
+    """
+    Creating a game screen.
+    """
     wn = turtle.Screen()
     wn.title("Ping Pong by @TokyoEdTech")
     wn.bgcolor("black")
     wn.setup(width=WIDTH, height=HEIGHT)
     wn.tracer(0)
 
+    """
+    Creating players.
+    """
     player1 = players.HumanPlayer(coordinates=(-350, 0))
     player2 = players.ComputerPlayer(coordinates=(350, 0))
-    # Ball
+
+    """
+    Creating a ball.
+    """
     ball = pingpong.Ball(coordinates=(0, 0))
 
-    # Pen
+    """
+    Pen used to write a score on the screen.
+    """
     pen = turtle.Turtle()
     pen.speed(0)
     pen.color("white")
@@ -46,25 +67,33 @@ def main():
     pen.goto(0, 260)
     show_score(pen, player1, player2)
 
-    # Keyboard binding
+    """
+    Keyboard bindings.
+    """
     wn.listen()
-    if player1.is_human():
+    if isinstance(player1, players.HumanPlayer):
         wn.onkeypress(player1.move_up, 'w')
         wn.onkeypress(player1.move_down, 's')
 
-    if player2.is_human():
+    if isinstance(player2, players.HumanPlayer):
         wn.onkeypress(player2.move_up, 'Up')
         wn.onkeypress(player2.move_down, 'Down')
 
-    # Main game
+    """
+    Main game loop.
+    """
     while True:
         wn.update()
 
-        # Move ball
+        """
+        Moving and speeding up the ball.
+        """
         ball.move()
         ball.speed_up()
 
-        # Border checking
+        """
+        Checking border collisions. 
+        """
         if ball.ycor() > HEIGHT / 2 - 10:
             os.system("aplay -q bounce.wav&")
             ball.sety(HEIGHT / 2 - 10)
@@ -85,20 +114,26 @@ def main():
             player2.score += 1
             show_score(pen, player1, player2)
 
-        # Paddle and ball
+        """
+        Checking paddles and the ball collisions.
+        """
         if ball.xcor() < 0:
             paddle_ball_collisions(player1, ball)
         else:
             paddle_ball_collisions(player2, ball)
 
-        # Computer movement
-        if not player1.is_human():
+        """
+        Computer move.
+        """
+        if isinstance(player1, players.ComputerPlayer):
             player1.computer_move(ball)
 
-        if not player2.is_human():
+        if isinstance(player2, players.ComputerPlayer):
             player2.computer_move(ball)
 
-        # End Game
+        """
+        Condition of the game end and instructions to do then. 
+        """
         if player1.score == WIN_SCORE or player2.score == WIN_SCORE:
             pen.clear()
             if player1.score == WIN_SCORE:
@@ -108,7 +143,7 @@ def main():
             time.sleep(5)
             break
 
-        time.sleep(0.01)
+        time.sleep(0.01)  # Thanks to that, game run same on any device.
 
 
 if __name__ == '__main__':
--- players.py
+++ players.py
@@ -21,27 +21,27 @@ class HumanPlayer(Player):
     def __init__(self, coordinates):
         super().__init__(coordinates)
 
-    def computer_move(self, ball):
-        pass
-
-    @staticmethod
-    def is_human():
-        return True
-
 
 class ComputerPlayer(Player):
 
     def __init__(self, coordinates):
         super().__init__(coordinates)
-        self.reaction_range = (60, 100)  # Easy: 10000, Medium: 1000, Hard: 100
 
+        '''
+        reaction_range variable has two number in pixel unit.
+        They are used in computer_move() method to generate random number from this range.  
+        '''
+        self.reaction_range = (60, 100)  # These two values are in pixel unit. Their meaning is if ball is
+
+    '''
+    This method is a 'brain' of computer in this game. 
+    Take an example for clarify reaction_range = (60, 100). In each frame of an animation, program generate
+    random number from (60, 100) range (let's say 'x'). If ball is 'x' pixels ahead or below of the paddle center, call
+    a proper method (move_up() or move_down()). The goal is ball 'y' and paddle 'y' coordinates are equal. 
+    '''
     def computer_move(self, ball):
         x, y = self.reaction_range
         if self.paddle.ycor() > ball.ycor() + random.uniform(x, y):
             self.move_down()
         elif self.paddle.ycor() < ball.ycor() - random.uniform(x, y):
             self.move_up()
-
-    @staticmethod
-    def is_human():
-        return False
